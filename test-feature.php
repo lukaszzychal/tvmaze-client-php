ca1cd0afdd323ee1cf1679646d149cad91760449
Łukasz Zychal <zychallukasz@gmail.com>
2025-10-15 13:27:49 +0200

    feat: add test feature for changelog generation
    - Add TestFeature class with test methods
    - This is a test commit to verify changelog generation works correctly
    - Includes testMethod() and anotherTestMethod() methods

---
+++ test-feature.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Test feature file for testing changelog generation
+ */
+
+class TestFeature {
+    
+    public function testMethod(): string
+    {
+        return 'This is a test feature for changelog generation';
+    }
+    
+    public function anotherTestMethod(): bool
+    {
+        return true;
+    }
+}
